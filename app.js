56a101716d93a6a3047d3f670b48fea82a24f389
German Gonzalez <german_ge9@hotmail.com>
2020-05-06 01:20:54 -0600

    Se agrego validación nombre archivo, colors para respuestas

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const argv = require('./config/yargs').argv;
-const fileReader = require('./file').FileReader;
+const fileReader = require('./file/file').FileReader;
+const colors = require('colors');
 
-const filename = './files/' + (argv._[1]?argv._[1]:'test.txt');
+const expValidarFileName = new RegExp('^[a-zA-Z0-9_,\s-]+\.[A-Za-z]{2,4}$');
 
-const file = new fileReader(filename);
+if(!expValidarFileName.test(argv._[1]))
+  return console.error('Introduzca un nombre de archivo valido'.red);
 
 if(argv._.length<2)
-  return console.error('Debe especificar el nombre del archivo a manipular');
+  return console.error('Debe especificar el nombre del archivo a manipular'.magenta);
 
 if(argv._.length>2)
-  return console.error('Solamente esta habilitado el comando sed, consulte --help');
+  return console.error('Verifique comando.\nSolamente esta habilitado el comando sed, para ayuda use --help'.red);
  
 switch(argv._[0]){
   case 'sed':
+    const filename = './files/' + (argv._[1]?argv._[1]:'test.txt');
+    const file = new fileReader(filename);
+
     if(argv.hasOwnProperty('rep') && argv.rep.length)
     {
-      file.replaceText(argv.rep);
+      console.log(file.replaceText(argv.rep).cyan);
     }
     else if(argv.hasOwnProperty('newln') && argv.newln){
-      file.newLine(argv.newln);
+      console.log(file.newLine(argv.newln).cyan);
     }
-    console.log(file.viewFile());
+    console.log(`${argv._[1]}*\n${ file.viewFile().green }`);
     break;
   default:
     console.log('Comando no existe, Para mas información use --help.');
