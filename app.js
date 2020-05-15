719296841d435de5ecdb909bd4c05369a8ad21a4
Gerson <gbumanzor@outlook.com>
2020-05-14 21:22:01 -0600

    :ok-hand: Pull Request.

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
-const argv = require('./config/yargs').argv;
-const fileReader = require('./file/file').FileReader;
-const colors = require('colors');
+const argumentList = require("./config/yargs").argv;
+const fileReader = require("./file/file").FileReader;
+const colors = require("colors");
 
-const expValidarFileName = new RegExp('^[a-zA-Z0-9_,\s-]+\.[A-Za-z]{2,4}$');
+const expValidarFileName = new RegExp("^[a-zA-Z0-9_,s-]+.[A-Za-z]{2,4}$");
 
-if(!expValidarFileName.test(argv._[1]))
-  return console.error('Introduzca un nombre de archivo valido'.red);
+if (!expValidarFileName.test(argumentList._[1]))
+  return console.error("Introduzca un nombre de archivo valido".red);
 
-if(argv._.length<2)
-  return console.error('Debe especificar el nombre del archivo a manipular'.magenta);
+if (argumentList._.length < 2)
+  return console.error(
+    "Debe especificar el nombre del archivo a manipular".magenta
+  );
 
-if(argv._.length>2)
-  return console.error('Verifique comando.\nSolamente esta habilitado el comando sed, para ayuda use --help'.red);
- 
-switch(argv._[0]){
-  case 'sed':
-    const filename = './files/' + (argv._[1]?argv._[1]:'test.txt');
+if (argumentList._.length > 2)
+  return console.error(
+    "Verifique comando.\nSolamente esta habilitado el comando sed, para ayuda use --help"
+      .red
+  );
+
+switch (argumentList._[0]) {
+  case "sed":
+    const filename =
+      "./files/" + (argumentList._[1] ? argumentList._[1] : "test.txt");
     const file = new fileReader(filename);
 
-    if(argv.hasOwnProperty('rep') && argv.rep.length)
-    {
-      console.log(file.replaceText(argv.rep).cyan);
+    if (argumentList.hasOwnProperty("rep") && argumentList.rep.length) {
+      console.log(file.replaceText(argumentList.rep).cyan);
+    } else if (argumentList.hasOwnProperty("newLine") && argumentList.newLine) {
+      console.log(file.newLine(argumentList.newLine).cyan);
     }
-    else if(argv.hasOwnProperty('newln') && argv.newln){
-      console.log(file.newLine(argv.newln).cyan);
-    }
-    console.log(`${argv._[1]}*\n${ file.viewFile().green }`);
+    console.log(`${argumentList._[1]}*\n${file.viewFile().green}`);
     break;
   default:
-    console.log('Comando no existe, Para mas información use --help.');
+    console.log("Comando no existe, Para mas información use --help.");
 }
